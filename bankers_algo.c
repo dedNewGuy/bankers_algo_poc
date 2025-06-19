b270183031846aa1fdef531883d4cfb6ec58efab
dedNewGuy <aminkafrinewgen@gmail.com>
2025-06-19 12:00:35 +0800

    implement bankers algo in c

---
+++ bankers_algo.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+/* Return the number of running process */
+int n_running_process(int running[], int length);
+int sum(int alloc_process[], int length);
+
+int main()
+{
+	int n_jobs = 0;
+	int n_resources = 1; // Only 1 resource type for simplicity sake
+
+	int available_res = 0;
+
+	printf("Number of Jobs: ");
+	scanf("%d", &n_jobs);
+
+	int alloc_process[n_jobs] = {};
+	int max_demand[n_jobs] = {};
+	int running[n_jobs];
+
+	for (int i = 0; i < n_jobs; ++i) {
+		running[i] = 1;
+	}
+
+	printf("Available Resources: ");
+	scanf("%d", &available_res);
+
+	for (int i = 0; i < n_jobs; ++i) {
+		printf("Allocated process for job %d: ", i + 1);
+		scanf("%d", &alloc_process[i]);
+	}
+
+	for (int i = 0; i < n_jobs; ++i) {
+		printf("Maximum demand for job %d: ", i + 1);
+		scanf("%d", &max_demand[i]);
+	}
+
+	int total_available = available_res - sum(alloc_process, n_jobs);
+	int at_least_one_allocate = 0;
+	while (n_running_process(running, n_jobs) > 0) {
+		at_least_one_allocate = 0;
+		for (int i = 0; i < n_jobs; ++i) {
+			if (running[i] == 1) {
+				if (total_available >= 0) {
+					at_least_one_allocate = 1;
+					printf("job %d is running...\n", i + 1);
+					running[i] = 0;
+					total_available += alloc_process[i];
+				}
+			}
+		}
+
+		if (at_least_one_allocate == 1) {
+			printf("Safe\n");
+		} else {
+			printf("Unsafe\n");
+		}
+	}
+		
+	return 0;
+}
+
+int n_running_process(int running[], int length)
+{
+	int nonzero_count = 0;
+	for (int i = 0; i < length; ++i) {
+		if (running[i] == 1)
+			nonzero_count++;
+	}
+
+	return nonzero_count;
+}
+
+int sum(int alloc_process[], int length)
+{
+	int sum = 0;
+	for (int i = 0; i < length; ++i) {
+		sum += alloc_process[i];
+	}
+
+	return sum;
+}
